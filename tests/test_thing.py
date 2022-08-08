fae3656c737901c27b01f3e239f1dc97920a17b3
ewssential <eikewilke1989@live.de>
2022-08-08 23:05:17 +0200

    very (un)intelligent stuff

--- tests/test_thing.py
+++ tests/test_thing.py
@@ -1,11 +1,55 @@
-from app.thing import Thing
+import random
+
+
+class EventSample(object):
+    def __init__(self):
+        self.__eventhandlersample = []
+
+    def __iadd__(self, Ehandler):
+        self.__eventhandlersample.append(Ehandler)
+        return self
+
+    def __isub__(self, Ehandler):
+        self.__eventhandlersample.remove(Ehandler)
+        return self
+
+    def __call__(self, *args, **keywargs):
+        for eventhandlersample in self.__eventhandlersample:
+            eventhandlersample(*args, **keywargs)
+
+
+class WeatherStation:
+    def __init__(self, station_type=None):
+        self.station_type = station_type
+        self.station_type.register(WeatherStation.update(self))
+
+    def update(self):
+        print(self.station_type.value)
+
+
+class TempType:
+    def __init__(self):
+        self.OnEH = EventSample()
+        self.type_name = "temp"
+        self.value = 0
+
+    def register(self, method):
+        self.OnEH += method
+
+    def new_measurement(self):
+        self.value = random.randint(10, 20)
+        self.OnEH()
 
 
 def test_correct_greeting():
-    thing = Thing("Bob")
-    assert "Hello Bob!" == thing.return_hello_name()
+    weather_station = WeatherStation(TempType())
+    exp = TempType().type_name
+    actual = weather_station.station_type.type_name
+    assert exp == actual
 
 
-def test_fail():
-    thing = Thing("Albert")
-    assert "Wrong!" == thing.return_hello_name()
+def test_other():
+    temp_type = TempType()
+    weather_station = WeatherStation(temp_type)
+    temp_type.new_measurement()
+    assert 1 == 2
