cf9315cabf3b3fd36961b662788aa4c929b37166
Eike Wilke <eike.wilke@mailbox.org>
2022-08-15 22:14:03 +0200

    create cell

--- tests/test_thing.py
+++ tests/test_thing.py
@@ -1,6 +1,17 @@
 from app.thing import Thing
 
 
-def test_correct_greeting():
-    thing = Thing()
-    assert False is True
+class Cell:
+    def __init__(self):
+        self.isAlive = False
+        self.coordinate = (0, 0)
+
+
+def test_create_cell_with_coordinated():
+    cell = Cell()
+    assert cell.coordinate is (0, 0)
+
+
+def test_created_cell_is_death():
+    cell = Cell()
+    assert cell.isAlive is False
