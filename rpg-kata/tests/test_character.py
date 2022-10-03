3dc8bda9fb5e5f90d4cf4ce0f2fcd2a1585af87e
ewssential <eikewilke1989@live.de>
2022-10-03 22:23:33 +0200

    Squashed commit of the following:
    commit 1416ee08c660c8a6033c02073663c56b1a653693
    Author: ewssential <eikewilke1989@live.de>
    Date:   Mon Oct 3 22:23:25 2022 +0200
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:rpg-kata/tests/test_character.py

--- rpg-kata/tests/test_character.py
+++ rpg-kata/tests/test_character.py
@@ -24,19 +24,24 @@ class RpgChar:
     def attack(self, enemy, damage):
         if enemy is self:
             return
+        return self.spam(damage, enemy)
+
+    def spam(self, damage, enemy):
         if enemy.Level - self.Level >= 5:
-            damage /= 2
-        enemy.get_attacked(damage)
-        return enemy
+            damage //= 2
+        if self.Level - enemy.Level >= 5:
+            damage = int(damage * 1.5)
+        return enemy.get_attacked(damage)
 
     def get_attacked(self, damage):
-        self.__suffer_damage(damage)
+        return self.__suffer_damage(damage)
 
     def __suffer_damage(self, damage):
         self.Health -= damage
         if self.Health <= 0:
             self.Health = 0
             self.Alive = False
+        return self
 
     def healed(self, healing):
         if not self.Alive:
@@ -132,10 +137,22 @@ def test_character_can_increase_level(rpg_char):
 def test_target_is_5_or_more_levels_above_the_attacker_damage_is_halved(rpg_char):
     target = RpgChar(level=6)
     rpg_char.attack(target, 100)
-    assert target.Health == 1000 - 100 / 2
+    assert target.Health == 1000 - 100 // 2
 
 
-def test_target_is_5_or_more_levels_above_the_attacker_odd_damage_is_halved(rpg_char):
+def test_target_is_5_or_more_levels_above_the_attacker_odd_damage_is_working(rpg_char):
     target = RpgChar(level=6)
     rpg_char.attack(target, 99)
-    assert target.Health == 1000 - 99 / 2
+    assert target.Health == 1000 - 99 // 2
+
+
+def test_target_is_5_or_more_levels_below_the_attacker_then_damage_is_50_percent_increased(rpg_char):
+    attacker = RpgChar(level=6)
+    attacker.attack(rpg_char, 100)
+    assert rpg_char.Health == 1000 - int(100 * 1.5)
+
+
+def test_target_is_5_or_more_levels_below_the_attacker_then_odd_damage_is_working(rpg_char):
+    attacker = RpgChar(level=6)
+    attacker.attack(rpg_char, 99)
+    assert rpg_char.Health == 1000 - int(99 * 1.5)
